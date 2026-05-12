3f015367bb5b9a9e7c8d77e28d160a55e2c755da
Alvin Sze <alvins44@nycstudents.net>
2026-05-12 12:34:48 -0400

    new files, basic code

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+import sqlite3
+import random
+from flask import Flask, render_template, session, request, redirect
+import os
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:red'>TEST SERVER!</h1>"
+app = Flask(__name__)
+app.secret_key = os.urandom(12)
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
+
+@app.route('/imposter', methods=["GET", "POST"])
+def imposter():
+    return render_template("imposter.html")
 
 if __name__ == "__main__":
     app.debug = False
